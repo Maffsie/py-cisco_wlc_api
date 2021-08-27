4f6a1460b99c594cf9f44b9c9c6913691711c066
Matthew Connelly <Matthew.Connelly@fanduel.com>
2021-08-27 15:18:56 +0100

    handle pylint stuff

--- test/test_import.py
+++ test/test_import.py
@@ -1,2 +1,15 @@
+"""
+Tests pertaining to basic imports
+"""
+
+
 def test_simple_import():
+    """Test that the module base can be imported"""
+    # pylint: disable=unused-import,import-outside-toplevel,import-error
     import src.cisco_wlc_api
+
+
+def test_simple_class_import():
+    """Test that the primary class can be imported"""
+    # pylint: disable=unused-import,import-outside-toplevel,import-error
+    from src.cisco_wlc_api import CiscoWLCAPI
--- test/test_wlc_constructor.py
+++ test/test_wlc_constructor.py
@@ -1,35 +1,52 @@
+"""
+Tests pertaining to the constructor for cisco_wlc_api.WLC.CiscoWLCAPI
+"""
+
+# pylint: disable=import-error
 import pytest
 from src.cisco_wlc_api import CiscoWLCAPI
 import src.cisco_wlc_api.Exceptions as CiscoWLCExceptions
 
 
 def test_constructor_requires_args():
+    """Test that the constructor correctly raises an exception
+    when there are no arguments"""
     with pytest.raises(CiscoWLCExceptions.WLCAssertionException):
         _ = CiscoWLCAPI()
 
 
 def test_constructor_requires_non_none_args():
+    """Test that the constructor correctly raises an exception
+    when the arguments are None"""
     with pytest.raises(CiscoWLCExceptions.WLCAssertionException):
         _ = CiscoWLCAPI(base_uri=None, credentials=None)
 
 
 def test_constructor_requires_credential_contents():
+    """Test that the constructor correctly raises an exception
+    when the credentials tuple is empty"""
     with pytest.raises(CiscoWLCExceptions.WLCAssertionException):
         _ = CiscoWLCAPI(base_uri='https://127.0.0.1', credentials=())
 
 
 def test_constructor_requires_credential_size():
+    """Test that the constructor correctly raises an exception
+    when the credentials tuple is of an incorrect size"""
     with pytest.raises(CiscoWLCExceptions.WLCAssertionException):
         _ = CiscoWLCAPI(base_uri='https://127.0.0.1', credentials=('1',))
     with pytest.raises(CiscoWLCExceptions.WLCAssertionException):
-        _ = CiscoWLCAPI(base_uri='https://127.0.0.1', credentials=('1','2','3'))
+        _ = CiscoWLCAPI(base_uri='https://127.0.0.1', credentials=('1', '2', '3'))
 
 
 def test_constructor_validates_arg_types():
+    """Test that the constructor correctly raises an exception
+    when the arguments are of an incorrect type"""
     with pytest.raises(CiscoWLCExceptions.WLCAssertionException):
         _ = CiscoWLCAPI(base_uri=int(1), credentials=str('1'))
 
 
 def test_constructor_validates_credential_types():
+    """Test that the constructor correctly raises an exception
+    when the credentials tuple contains incorrect types"""
     with pytest.raises(CiscoWLCExceptions.WLCAssertionException):
         _ = CiscoWLCAPI(base_uri='https://127.0.0.1', credentials=(int(2), ('3',)))
